51fea343bc8b08c3d366458ea68d05d5b13eef99
muziga judith <umuzigajudit@gmail.com>
2025-10-03 13:38:52 +0200

    The function fetchAuthorsSithArticles retrieves posts along with their authors and comments, then organizes the data by author, grouping their articles and associated comments together
     Added async function to retrieve posts with embedded comments and expanded user data.
    - Grouped posts by author ID to build a structured author object.
    - For each author, stored their basic info and a list of their articles.
    - Each article includes its comments (id and content).
    - Returned a clean array of authors with their articles and comments.

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 // QN1
 //By use of function
-
-const { useId } = require("react");
-
-
-async function fetchAuthorswithArticles() {
-
-   const res = await fetch('https://jsonplaceholder.typicode.com/posts?_embed=comments&_expand=user');
-const posts = await res.json();
-
-const  author ={}
-
-posts.forEach(post => {
-  const userId = post.user.id;
-  
-  if (!author[userId]) {
-    author[userId] = {
-      id: post.user.id,
-      name: post.user.name,
-      articles: []
-    };
-    author[userId].articles.push(
-        {
+async function fetchAuthorsSithArticles(){
+    const response=await fetch("https://jsonplaceholder.typicode.com/posts?_embed=comments&_expand=user");
+    const posts=await response.json();
+    const author={};
+    posts.forEach(post=>{
+        const userId=post.user.id;
+        author[userId]={
+            id:post.user.id,
+            name:post.user.name,
+            articles:[]
+        }
+author[userId].articles.push({
             id:post.id,
             title:post.title,
-        comments:post.comments.map(c=> ({
-            id:c.id,
-            content.c.body,
-        }
-            ))
-
-        }
-    )
-    const authorsArray=Object.values(author);
+            body:post.body,
+            comments:post.comments.map(c=>({
+                id:c.id,
+                content:c.body
+            })),
+});
+    }
+)
+const authorsArray=Object.values(author);
 return authorsArray;
-  }
-  fetchAuthorsSithArticles().then(authors=>{
+}
+fetchAuthorsSithArticles().then(authors=>{
     console.log(JSON.stringify(authors,null,2));
 });
-}
+
+
+
+
+
+
+
+
+
+
+
+
